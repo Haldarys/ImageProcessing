1ca5703e74176b67a0a4c098a5def656af347740
Haldarys <jp45230@gmail.com>
2021-10-09 23:54:41 +0200

    Ajout des modifs

--- src/tpHistogram.cpp
+++ src/tpHistogram.cpp
@@ -16,7 +16,7 @@ Mat inverse(Mat image)
       /********************************************
                 YOUR CODE HERE
     *********************************************/
-    
+    res = 1.0 - res;
     /********************************************
                 END OF YOUR CODE
     *********************************************/
@@ -37,7 +37,15 @@ Mat threshold(Mat image, float lowT, float highT)
     /********************************************
                 YOUR CODE HERE
     *********************************************/
-    
+    for(int y = 0; y < res.rows; y++) {
+        for(int x = 0; x < res.cols; x++) {
+                if(res.at<float>(y, x) <= lowT){
+                    res.at<float>(y, x) = 0;
+                } else if (res.at<float>(y, x) >= highT) {
+                    res.at<float>(y, x) = 1;
+                }
+        }
+    }
     /********************************************
                 END OF YOUR CODE
     *********************************************/
@@ -58,7 +66,18 @@ Mat quantize(Mat image, int numberOfLevels)
     /********************************************
                 YOUR CODE HERE
     *********************************************/
-    
+    for(int y = 0; y < res.rows; y++) {
+        for(int x = 0; x < res.cols; x++) {
+            float step = 1.0f / numberOfLevels;
+            for(int lvl = 0; lvl < numberOfLevels; lvl++) {
+                float lvlMaxVal = ((float)lvl+1) * step;
+                float lvlMinVal = (float)lvl * step;
+                if(res.at<float>(y, x) < lvlMaxVal && res.at<float>(y, x) >= lvlMinVal) {
+                    res.at<float>(y, x) = (1.0f / (numberOfLevels-1)) * lvl; //step * lvl;
+                }
+            }
+        }
+    }
     /********************************************
                 END OF YOUR CODE
     *********************************************/
@@ -76,7 +95,24 @@ Mat normalize(Mat image, float minValue, float maxValue)
     /********************************************
                 YOUR CODE HERE
     *********************************************/
-    
+    float fmin = 1;
+    float fmax = 0;
+    for(int y = 0; y < res.rows; y++) {
+        for(int x = 0; x < res.cols; x++) {
+            if(res.at<float>(y, x) < fmin){
+                fmin = res.at<float>(y, x);
+            }
+            if(res.at<float>(y, x) > fmax){
+               fmax = res.at<float>(y, x); 
+            }
+        }
+    }
+    for(int y = 0; y < res.rows; y++) {
+        for(int x = 0; x < res.cols; x++) {
+            res.at<float>(y, x) = (res.at<float>(y, x) - fmin) * ((maxValue - minValue) / (fmax - fmin)) + minValue;
+        }
+    }
+
     /********************************************
                 END OF YOUR CODE
     *********************************************/
